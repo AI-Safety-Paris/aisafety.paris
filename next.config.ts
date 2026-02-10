bf68e63cf13508534c83c66b8b7493d3723210f5
Lucie Philippon <lucie.philippon@proton.me>
2026-02-10 14:18:55 +0100

    IASEAI side event spreadsheet shortlink

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  redirects: async () => {
+    return [
+      {
+        source: "/iaseai-26-side-events",
+        destination: "https://docs.google.com/spreadsheets/d/1o9Q4ais95h7VwZbhugf7urC_l1P_T5ML4h0KEoPvrSk/edit?gid=0#gid=0",
+        permanent: true,
+      },
+    ];
+  }
 };
 
 export default nextConfig;
