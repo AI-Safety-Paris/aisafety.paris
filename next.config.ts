21a8aba69cb8f126eee6a1b3e0519e79017ac638
Lucie Philippon <lucie.philippon@proton.me>
2026-02-10 14:20:08 +0100

    non permanent redirect

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
       {
         source: "/iaseai-26-side-events",
         destination: "https://docs.google.com/spreadsheets/d/1o9Q4ais95h7VwZbhugf7urC_l1P_T5ML4h0KEoPvrSk/edit?gid=0#gid=0",
-        permanent: true,
+        permanent: false,
       },
     ];
   }
